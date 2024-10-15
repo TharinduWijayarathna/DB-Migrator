e28dfaeb73adf919a91cc1477561147309ae2d4f
TharinduWijayarathna <wikum.dev@gmail.com>
2024-10-16 03:32:43 +0530

    Layout Changes

--- resources/js/Pages/DBConnection/Index.vue
+++ resources/js/Pages/DBConnection/Index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Breadcrumb from '@/Components/Breadcrumb.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 
@@ -22,10 +23,193 @@ const breadcrumbItems = [
             <Breadcrumb :items="breadcrumbItems" />
         </template>
 
-        <div class="py-12">
-            <div class="mx-auto sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+        <div class="py-5 text-end sm:px-6 lg:px-8">
+            <PrimaryButton :href="route('connection.create')"
+                >Add Connection</PrimaryButton
+            >
+        </div>
+
+        <div class="mx-auto sm:px-6 lg:px-8">
+            <div
+                class="overflow-hidden bg-white p-5 shadow-sm sm:rounded-lg sm:px-6"
+            >
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead>
+                        <tr>
+                            <th
+                                class="bg-gray-50 px-2 py-3 text-left text-xs font-medium uppercase leading-4 tracking-wider text-gray-500"
+                            >
+                                Name
+                            </th>
+                            <th
+                                class="bg-gray-50 px-2 py-3 text-left text-xs font-medium uppercase leading-4 tracking-wider text-gray-500"
+                            >
+                                Host
+                            </th>
+                            <th
+                                class="bg-gray-50 px-2 py-3 text-left text-xs font-medium uppercase leading-4 tracking-wider text-gray-500"
+                            >
+                                Port
+                            </th>
+                            <th
+                                class="bg-gray-50 px-2 py-3 text-left text-xs font-medium uppercase leading-4 tracking-wider text-gray-500"
+                            >
+                                Database
+                            </th>
+                            <th class="bg-gray-50 px-6 py-3"></th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200 bg-white">
+                        <tr>
+                            <td class="whitespace-no-wrap px-2 py-4">
+                                <div class="text-sm leading-5 text-gray-900">
+                                    Localhost
+                                </div>
+                            </td>
+                            <td class="whitespace-no-wrap px-2 py-4">
+                                <div class="text-sm leading-5 text-gray-900">
+                                    localhost
+                                </div>
+                            </td>
+                            <td class="whitespace-no-wrap px-2 py-4">
+                                <div class="text-sm leading-5 text-gray-900">
+                                    3306
+                                </div>
+                            </td>
+                            <td class="whitespace-no-wrap px-2 py-4">
+                                <div class="text-sm leading-5 text-gray-900">
+                                    test_db
+                                </div>
+                            </td>
+                            <td
+                                class="whitespace-no-wrap px-2 py-4 text-right text-sm font-medium leading-5"
+                            >
+                                <a
+                                    href="#"
+                                    class="text-indigo-600 hover:text-indigo-900"
+                                    >Edit</a
+                                >
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div
+                    class="flex items-center justify-between border-t border-gray-200 bg-white py-3"
+                >
+                    <div class="flex flex-1 justify-between sm:hidden">
+                        <a
+                            href="#"
+                            class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                        >
+                            Previous
+                        </a>
+                        <a
+                            href="#"
+                            class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                        >
+                            Next
+                        </a>
+                    </div>
+                    <div
+                        class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between"
+                    >
+                        <div>
+                            <p class="pl-1 text-sm text-gray-700">
+                                Showing
+                                <span class="font-medium">1</span>
+                                to
+                                <span class="font-medium">10</span>
+                                of
+                                <span class="font-medium">97</span>
+                                results
+                            </p>
+                        </div>
+                        <div>
+                            <nav
+                                class="isolate inline-flex -space-x-px rounded-md shadow-sm"
+                                aria-label="Pagination"
+                            >
+                                <a
+                                    href="#"
+                                    class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                                >
+                                    <span class="sr-only">Previous</span>
+                                    <svg
+                                        class="h-5 w-5"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                                            clip-rule="evenodd"
+                                        />
+                                    </svg>
+                                </a>
+                                <a
+                                    href="#"
+                                    aria-current="page"
+                                    class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    1
+                                </a>
+                                <a
+                                    href="#"
+                                    class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                                >
+                                    2
+                                </a>
+                                <a
+                                    href="#"
+                                    class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                                >
+                                    3
+                                </a>
+                                <span
+                                    class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0"
+                                >
+                                    ...
+                                </span>
+                                <a
+                                    href="#"
+                                    class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                                >
+                                    8
+                                </a>
+                                <a
+                                    href="#"
+                                    class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                                >
+                                    9
+                                </a>
+                                <a
+                                    href="#"
+                                    class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                                >
+                                    10
+                                </a>
+                                <a
+                                    href="#"
+                                    class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                                >
+                                    <span class="sr-only">Next</span>
+                                    <svg
+                                        class="h-5 w-5"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                                            clip-rule="evenodd"
+                                        />
+                                    </svg>
+                                </a>
+                            </nav>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
